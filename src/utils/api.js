c91f05f4ed521dda0c6ae43559d6746c2de164a9
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2022-09-04 20:30:36 -0300

    Archivo .env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "bcb046476fmshfe8b5f3c6ba162bp19cf06jsnc73315db109a",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_WEATHER_API_KEY,
+    "X-RapidAPI-Host": process.env.REACT_APP_WEATHER_API_URL,
   },
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL;
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "c322e9c2b42cb86b8339abe7299c86dd";
+export const WEATHER_API_URL = process.env.REACT_APP_RAPIDAPI_HOST;
+export const WEATHER_API_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
