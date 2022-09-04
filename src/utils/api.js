264b1ee63727a35f1386f7ba9e8eeb0e1dee61c0
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2022-09-04 20:35:01 -0300

    Correcciones

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_WEATHER_API_KEY,
-    "X-RapidAPI-Host": process.env.REACT_APP_WEATHER_API_URL,
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
+    "X-RapidAPI-Host": process.env.REACT_APP_RAPIDAPI_HOST,
   },
 };
 
 export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL;
 
-export const WEATHER_API_URL = process.env.REACT_APP_RAPIDAPI_HOST;
-export const WEATHER_API_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
+export const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL;
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
